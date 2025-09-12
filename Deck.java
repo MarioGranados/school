7e0d3c4ac9530c28e56e8483471e98b19749707f
Mario Granados <mariogranados956@gmail.com>
2025-09-11 22:28:52 -0500

    deck stuff

---
+++ Deck.java
@@ -0,0 +1,27 @@
+public class Deck implements DeckInterface {
+
+    @Override
+    public void push(String card) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public String pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public void shuffle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shuffle'");
+    }
+    
+}
---
+++ DeckInterface.java
@@ -0,0 +1,7 @@
+
+public interface DeckInterface {
+    void push(String card);
+    String pop();
+    boolean isEmpty();
+    void shuffle();
+}
