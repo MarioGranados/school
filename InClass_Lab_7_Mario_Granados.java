1bca073f23584b38b61a6cfcde5bacb529e4dc3a
Mario Granados <mariogranados956@gmail.com>
2025-10-17 23:45:02 -0500

    creating a hashmap collision

---
+++ InClass_Lab_7_Mario_Granados.java
@@ -0,0 +1,80 @@
+public class InClass_Lab_7_Mario_Granados {
+    public static class HashTable {
+        private Integer[] table;
+        private int size;
+
+        public HashTable(int capacity) {
+            table = new Integer[capacity];
+            size = capacity;
+        }
+
+        public void put(Integer value) {
+            int hash = value % size; //begin hash code
+            int originalHash = hash;
+
+            while (table[hash] != null) {
+                // if the spot is taken move to the next spot
+                hash = (hash + 1) % size;
+                // if table full return
+                if (hash == originalHash) {
+                    System.out.println("cannot insurt value:  " + value + " table is full");
+                    return;
+                }
+            }
+
+            table[hash] = value;
+        }
+
+        // find by the key
+        // if hash = key then we return that value, otherwise assume
+        // we moved to the next index so check the next index until found.
+        public Integer get(Integer key) {
+            int hash = key % size;
+            int originalHash = hash;
+
+            while (table[hash] != null) {
+                if (table[hash].equals(key)) {
+                    return table[hash];
+                }
+                hash = (hash + 1) % size;
+                if (hash == originalHash) break;  //ran into infinite loop without this 
+            }
+            return null;
+        }
+
+        // was running into hexdecimal when converting to string
+        // found this solution online and tweaked it
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (int i = 0; i < size; i++) {
+                sb.append(table[i]);
+                if (i < size - 1) sb.append(", ");
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+
+
+    public static class Main {
+        public static void main(String[] args) {
+            HashTable table = new HashTable(10);
+
+            table.put(0);
+            table.put(1);
+            table.put(2);
+            table.put(3);
+            table.put(4);
+            table.put(10);
+            table.put(9);
+            table.put(19);
+
+            System.out.println(table);
+            System.out.println(table.get(9));
+            System.out.println(table.get(10));
+            System.out.println(table.get(11));
+        }
+    }
+}
